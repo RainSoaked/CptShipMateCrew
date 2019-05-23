1ba9c9b4650c50c61ef7ee4eba5c4e1ce3588cc0
RainSoaked <50919346+RainSoaked@users.noreply.github.com>
2019-05-22 21:42:52 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -14,7 +14,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static ImageView imageView1;
+    public static ImageView imageView1, imageView2, imageView3, imageView4, imageView5;
     public static Handler mainHandler = new Handler();
     Button rollDie;
 
@@ -26,7 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView4 = (ImageView) findViewById(R.id.imageView4);
+        imageView5 = (ImageView) findViewById(R.id.imageView5);
 
+        //need a counter to track 3 roll attempts; end game; total points
         rollDie = findViewById(R.id.rollDie);
         rollDie.setOnClickListener( new View.OnClickListener() {
 
@@ -34,7 +39,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Runnable die1 = new ShipDriver(0);
+                Runnable die2 = new ShipDriver(1);
+                Runnable die3 = new ShipDriver(2);
+                Runnable die4 = new ShipDriver(3);
+                Runnable die5 = new ShipDriver(4);
                 die1.run();
+                die2.run();
+                die3.run();
+                die4.run();
+                die5.run();
 
 
             }
